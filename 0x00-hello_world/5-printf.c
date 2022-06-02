a30802b85d42126a97b0218cbf3d9d8449e3eb17
Samsonasumu <samsonasumu@gmail.com>
2022-06-02 05:26:16 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/***
+ *
+ *
+ *
+ *
+ * some betty stle comment to show start of execution of my programm**/
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art\n");
+	return(0);
+}
