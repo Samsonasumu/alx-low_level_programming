818da5358c3e77b900f54f5ebfb2f926d35b1d6c
Samsonasumu <samsonasumu@gmail.com>
2022-06-03 08:48:34 -0700

    2. I sometimes suffer from inso

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *
+ * main first prints alphabets in lowercase then in capital letters
+*/
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+ putchar(alph);
+}
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+putchar(toupper(alph));
+}
+putchar('\n');
+return (0);
+}
+
