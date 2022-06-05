4804a97c27692d38647ee0fca789c32c555a2845
Samsonasumu <samsonasumu@gmail.com>
2022-06-05 07:04:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
+/***main function is the start of the execution 
  *
- *
- * betty comment style
- */
-
+ * always return 0 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
