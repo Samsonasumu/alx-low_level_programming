af0dadd1d6d6d0f023b6750bddccf5962e9ae0fd
Samsonasumu <samsonasumu@gmail.com>
2022-06-06 03:46:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-/*
- * Main is the start of excecution
- * Return 0 always
- * /
+/**
+ * Main - is the start of excecution
+ * Return - always 1 if succes
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
