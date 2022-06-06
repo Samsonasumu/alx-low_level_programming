87c09b4c75466655bfc982c114d73503bcaffcc3
Samsonasumu <samsonasumu@gmail.com>
2022-06-06 03:49:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * Main - is the start of excecution
- * Return - always 1 if succes
- */
+ * Return - always 1 if success
+*/
 int main(void)
 {
 	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
