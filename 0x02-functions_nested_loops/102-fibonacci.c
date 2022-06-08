ebf427dd3959c221683e3c8ca8e65a6d61554914
Samsonasumu <samsonasumu@gmail.com>
2022-06-08 09:55:22 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonucci starting with one and two followe
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i, j, k, next;
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+
+	return (0);
+}
