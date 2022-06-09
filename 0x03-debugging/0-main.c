d48a3cc24fe620ca39bbd29cde2461a48e0613b1
Samsonasumu <samsonasumu@gmail.com>
2022-06-09 00:22:27 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the number and determine whether positve or negative
+ * Return: Always 0
+ */
+int main(void)
+{
+	int k;
+
+	k = 65;
+	positive_or_negative(k);
+
+	return (0);
+}
