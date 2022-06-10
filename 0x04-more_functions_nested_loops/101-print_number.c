956223a00af87b05205061cf7c78745001903684
Samsonasumu <samsonasumu@gmail.com>
2022-06-10 09:34:53 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,27 +6,20 @@
  */
 void print_number(int n)
 {
-	unsigned int x, d, count;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		x = n * -1;
+		n1 = -1;
+		_putchar('-');
 	}
 	else
 	{
-		x = n;
+		n1 = n;
 	}
-	d = x;
-	count = 1;
-
-	while (d > 9)
+	if (n1 / 10)
 	{
-		d /= 10;
-		count *= 10;
+		print_number(n1 / 10);
 	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((x / count) % 10) + 48);
-	}
+	_putchar((n1 % 10) + '0');
 }
