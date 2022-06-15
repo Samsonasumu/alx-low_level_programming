7624acb5d760978b08423b8fe1c33b3ed99f2a37
Samsonasumu <samsonasumu@gmail.com>
2022-06-15 03:09:32 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unstd.h>
+/**
+ * _putchar - writes char c to output
+ * @c: character to print
+ * Return: on suceess 1 on error -1
+ */
+int _putchar(char c)
+{
+	return (write(1, $c, 1));
+}
