9c072ad4c3539f18dcc16a8de7c2de0a28d3fcbc
Samsonasumu <samsonasumu@gmail.com>
2022-06-16 04:51:21 -0700

    updated 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,11 +11,11 @@ char *leet(char *s)
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) '\0')
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == up_letters[i])
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 			{
 				*(s + count) = numbers[i];
 				break;
