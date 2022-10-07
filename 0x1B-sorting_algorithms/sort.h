e56501ec897a4fe9410f02528adc5a0201652de2
Samsonasumu <samsonasumu@gmail.com>
2022-10-07 09:24:19 -0700

    fix

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+fndef _SORT_
+#define SORT_
 
 #include <stdlib.h>
 /**
@@ -31,5 +31,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 #endif
